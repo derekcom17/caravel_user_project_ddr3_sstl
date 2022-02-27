78d91463f02839dce876db3dcbee0c1adce27259
Derek H-M <derekcom16@gmail.com>
2022-02-26 16:36:20 -0800

    Add top level user_project_wrapper cell
    * Add SSTL test circuit and wire up most signals

--- mag/SSTL/pex_netgen.tcl
+++ mag/SSTL/pex_netgen.tcl
@@ -1,10 +1,23 @@
-extract all;
-ext2spice lvs;
-# ext2spice hierarchy off;
-ext2spice cthresh 0.01
-ext2spice rthresh 1
-ext2spice subcircuit on;
+# Tim's Cap-Res extraction procedure
+
+set NAME [cellname list self];
 select top cell;
-ext2spice -o "pex_[cellname list self].spice";
-# Replace names with:
-# sed -i "s/sky130_fd_sc_hd__/extr_sky130_fd_sc_hd__/g" lvs_SSTL.spice
+# (Flattening improves resistance extraction)
+flatten "[list $NAME]_flat";
+load "[list $NAME]_flat";
+select top cell;
+
+extract do resistance;
+extract all;
+
+ext2sim labels on;
+ext2sim;
+
+extresist tolerance 10;
+extresist all;
+
+ext2spice lvs;
+ext2spice cthresh 0.1;
+ext2spice rthresh 10
+ext2spice extresist on;
+ext2spice -o "pex_[list $NAME].spice";
